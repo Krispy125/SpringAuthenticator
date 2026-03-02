4b7ddcf1155bac30f0d097d8193bf4a95215723b
Krispy125 <kburns19@rockets.utoledo.edu>
2026-03-01 20:39:15 -0500

    Added home controller to display main page text.

--- src/main/java/com/kris/authenticator/HomeController.java
+++ src/main/java/com/kris/authenticator/HomeController.java
@@ -1,5 +1,12 @@
 package com.kris.authenticator;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class HomeController {
-    
+    @GetMapping("/")
+    public String index() {
+        return "Welcome!!!";
+    }
 }
